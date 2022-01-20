9b20f23ade94d708cde7ba847c4272ea69dc8732
NadiiaHonchar <17nmg82@gmail.com>
2022-01-20 15:38:48 +0200

    added conditions

--- src/App.js
+++ src/App.js
@@ -45,16 +45,18 @@ class App extends Component {
     }));
   };
 
-  componentDidMount(){
-    this.setState({contacts:JSON.parse(localStorage.getItem('contacts'))});
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.contacts!== prevState.contacts)
-    {
-      localStorage.setItem('contacts',JSON.stringify(this.state.contacts));
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
     }
-  };
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
